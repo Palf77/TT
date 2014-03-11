8327d782f88104ccc3f51969ed8763242766155e
Christophe Olivier <christophe@Mac-mini-de-Christophe.local>
2014-03-11 14:16:55 +0100

    Initial Commit

---
+++ fr.test1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  fr.test1
+//
+//  Created by Christophe Olivier on 11/03/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ fr.test1Tests/fr_test1Tests.m
@@ -0,0 +1,34 @@
+//
+//  fr_test1Tests.m
+//  fr.test1Tests
+//
+//  Created by Christophe Olivier on 11/03/2014.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fr_test1Tests : XCTestCase
+
+@end
+
+@implementation fr_test1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
